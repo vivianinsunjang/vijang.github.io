3ce2b71367105b8527c4355b627dc5eb1ee9c4c9
vijang <50184863+vijang@users.noreply.github.com>
2021-02-08 22:12:14 -0800

    pushing all files  from local

---
+++ work/work.js
@@ -0,0 +1,144 @@
+work=[
+
+         {  "folder":"hologram",
+    "name":"Window in a Den",
+    "tags":"Augmented Reality, Hologram, Unity, Blender",
+    "thumb":"thumb.jpg",
+    "content":`2020
+    working from home 
+    since March 
+    from my windowless den
+    [pano without.jpg]
+
+    parked in front of my screen
+    facing a wall
+    [back.jpg]
+   
+    missing the office windows
+    the natural scenery outside 
+    brief serenity offered by the trees and sky
+    between meeting rooms
+    [microsoft windows.jpg]
+
+    I wanted to recreate this feeling 
+    create a scene beyond my wall
+    A portal to transport me
+    An augmented window in my office 
+    [side w hologram.jpg]
+    `
+  },
+    
+             {  "folder":"AOT",
+    "name":"As One Thinketh",
+    "tags":"Writing, Inclusion",
+    "thumb":"thumb.jpg",
+    "content":`One time I came across this book
+    [AAMT.png]
+    Not long after I started reading, I noticed something
+    [masc.gif]
+    the book used exclusively masculine words.
+    Triggered, I immediately went to work,
+    
+    [triggered.gif]
+    
+    replacing the masculine words with feminine ones.
+    [table.jpeg]
+
+    I created a new version of the book,
+    for women.
+    [AAWT.png]
+    Content with my handiwork, I was ready to declare victory and move on.
+   
+    And yet... 
+
+
+    
+    I felt I was missing something.
+   
+    I went to find out more about the author. 
+    [google.gif]
+    [wiki.gif]
+    This book was written 100+ years ago in 1903?!
+    I started to feel silly about my triggered reaction.
+
+    Was I unfairly judging an old book through today's lens?
+
+    
+ 
+    I even found this quote from the author's wife:
+    [quote.jpg]
+
+    Perhaps I could've given the author the benefit of the doubt
+    I dug deeper to see which words were attached to the masculine pronouns.
+
+    [HisHer.jpeg]
+
+    The words seemed to be relevant to all people, not just men!
+
+    And then I noticed it.
+    [AAWT foreward.png]
+
+    I was originally triggered 
+    because I felt excluded from the audience as a woman.
+
+    And yet 
+    my solution was also excluding others!
+
+    So I tried again
+    [AOT.png]
+    [AOT foreward.png]
+
+        `
+  }
+    
+
+
+/*,
+    
+     {  "folder":"M3",
+    "name":"M3",
+    "tags":"Collage, Mixed Media",
+    "thumb":"thumb.jpg",
+    "content":`I'm interested in what happens to language under specific sociopolitical constraints.
+    Language under capitalism compresses itself—it minifies into lean, neat boxes fit for work and production. It does not rebel, it does not deviate; it eclipses possibility of what could be in our relationships.
+    <iframe src="https://player.vimeo.com/video/469486919" width="100%" height="396" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+    This interactive, audio-based web poem plays out through a fictional bureaucratic phone call. <a href="office/index.html" target="_blank">Try it on your mobile device</a> for the intended experience.
+    [phonedemo-title.gif]
+    Dialing any one of several digits (including secret, non-menu digits) will play a computer-synthesized, female voice recording.
+    [2.jpg]
+    [3a.jpg]
+    [3b.jpg]
+    [4.jpg]
+    [5.jpg]
+    [6.jpg]
+    Most of this text was written by a human pretending to be a machine; some text was generated through Python webscrapes of Google autocompletes.
+    [8.jpg]
+    In total, I webscraped over 3,000 discrete expressions of Thank You. (I've only used a subset)
+    [thankyou.jpg]
+
+    Finally, no bureaucratic call is complete without some palatable music.
+    [0.jpg]
+    
+    `
+  },
+
+
+     {  "folder":"GxK",
+    "name":"Germany x Korea",
+    "tags":"Sliced Sculpture, Laser Cut",
+    "thumb":"thumb.jpg",
+    "content":`An <a href="copyandpaste/index.html" target="_blank">interactive web poem</a> I wrote and coded about Chinese bodies in a context of mass production.
+    Works on mobile. For a more spatial viewing experince, try on a desktop computer.
+    [layering.gif]
+    [1.jpg]
+    [2.jpg]
+    [4.jpg]
+    [7.jpg]
+    [12.jpg]
+    Here's the experience on a mobile device:
+    [phonegif.gif]
+    Images from DuckDuckGo. Font is <a href="https://fonts.google.com/specimen/Piazzolla?category=Serif" target="_blank">Piazolla</a> by Google Fonts.
+
+    `
+  }*/
+    ]
